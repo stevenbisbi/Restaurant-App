c873600d2872a45e09a0b9fa534410b60793ac5f
Michael <stevenex13@gmail.com>
2025-03-25 12:14:06 -0500

    initial commit

---
+++ usuarios/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+import uuid
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class Usuario(AbstractUser):
+    email = models.EmailField(unique=True)
+
+    """ def __str__(self):
+        return f'{self.nombre} {self.apellido}' """
