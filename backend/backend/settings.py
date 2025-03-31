94bd3fd4fd9afe7cbe3e30ab0df2520edd95e856
Michael <stevenex13@gmail.com>
2025-03-30 22:56:56 -0500

    fix: appsname2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'menu',
+    'notifications',
+    'orders',
+    'payments',
+    'reservations',
+    
 ]
 
 MIDDLEWARE = [
