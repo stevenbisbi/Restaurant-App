e1a600aa3692622ce6f5b57628f3a2f93bd3c6f4
Michael <stevenex13@gmail.com>
2025-04-06 16:15:26 -0500

    prueba

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+export function Footer() {
+  return (
+    <footer>
+      <div className="container">
+        <h1 className="text-center">RestaurantOS</h1>
+        <div className="d-flex justify-content-center">
+          <p className="mx-3">&copy; 2025 Parcha2.</p>
+          <p className="mx-3">Todos los derechos reservados. </p>
+          <p className="mx-3">Michael Steven Sanchez</p>
+        </div>
+      </div>
+    </footer>
+  );
+}
