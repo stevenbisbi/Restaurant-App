c3522d0d57617e3670c9770b5bfe5fa2ac35e13d
Michael <stevenex13@gmail.com>
2025-05-01 16:49:32 -0500

    bugfix 2

--- backend/notifications/models.py
+++ backend/notifications/models.py
@@ -5,11 +5,7 @@ from users.models import User
 
 class Notification(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-<<<<<<< HEAD
     user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name="notifications")
-=======
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notifications")
->>>>>>> dfc400d6057bd966c93099f7736d4b1afa04efbe
     title = models.CharField(max_length=100)
     message = models.TextField()
     type = models.CharField(max_length=50)
