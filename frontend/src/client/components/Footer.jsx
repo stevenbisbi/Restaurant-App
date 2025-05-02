3e341e4bf400a072e42504ad901a4a6cf14a7922
Michael <stevenex13@gmail.com>
2025-05-01 21:27:24 -0500

    style: footer fixed style

--- frontend/src/client/components/Footer.jsx
+++ frontend/src/client/components/Footer.jsx
@@ -1,20 +1,12 @@
 export function Footer() {
   return (
-    <footer
-      style={{
-        display: "grid",
-        minHeight: "100dvh",
-        gridTemplateRows: "auto 1fr auto",
-        padding: "20px",
-        backgroundColor: "#f8f9fa",
-      }}
-    >
-      <div className="container">
-        <h1 className="text-center">RestaurantOS</h1>
-        <div className="d-flex justify-content-center">
-          <p className="mx-3">&copy; 2025 Parcha2.</p>
-          <p className="mx-3">Todos los derechos reservados. </p>
-          <p className="mx-3">Michael Steven Sanchez</p>
+    <footer>
+      <div className="d-flex align-items-center py-1 justify-content-between px-5">
+        <h1>RestaurantOS</h1>
+        <div className="info-footer d-flex">
+          <p className="mx-2">&copy; 2025 Parcha2.</p>
+          <p className="mx-2">Todos los derechos reservados. </p>
+          <p className="mx-2">Michael Steven Sanchez</p>
         </div>
       </div>
     </footer>
