55e09e9b7633e1742bd27ebe46ea7c047fa01f70
Kevin Manrique <krmanriquesanchez@gmail.com>
2025-05-03 00:58:32 -0500

    feat: implement reservation CRUD with table and status relations

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-from dotenv import load_dotenv
+from dotenv import load_dotenv # type: ignore
 import os
 
 load_dotenv()
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -40,6 +40,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/reservations/', include('reservations.urls')),
+    path('api/restaurant/', include('restaurant.urls')),
 
     # Swagger
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
