fcac7f620f0c7785fcada11c2677de95ef815af3
Michael <stevenex13@gmail.com>
2025-05-18 16:09:49 -0500

    auth folder

--- backend/restaurant/admin.py
+++ backend/restaurant/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Restaurant, Permission, Role
 # Register your models here.
+
+admin.site.register(Restaurant)
+admin.site.register(Permission)
+admin.site.register(Role)
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Customer, Staff, User
 # Register your models here.
+
+admin.site.register(Customer)
+admin.site.register(Staff)
+admin.site.register(User)
