a8fcf31ade7f9ba7e924d1c912f43e4131285265
Michael <stevenex13@gmail.com>
2025-05-18 15:33:07 -0500

    try superuser

--- backend/menu/admin.py
+++ backend/menu/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Menu, MenuCategory, MenuItem, MenuItemOption, MenuItemVariant
 # Register your models here.
+
+admin.site.register(Menu)
+admin.site.register(MenuCategory)
+admin.site.register(MenuItem)
+admin.site.register(MenuItemOption)
+admin.site.register(MenuItemVariant)
