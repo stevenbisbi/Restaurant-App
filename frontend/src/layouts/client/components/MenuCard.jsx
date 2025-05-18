82d766ee4250963b6f4d46e0498aa8e3528918b5
Michael <stevenex13@gmail.com>
2025-05-18 02:57:49 -0500

    styles menu card

--- frontend/src/layouts/client/components/MenuCard.jsx
+++ frontend/src/layouts/client/components/MenuCard.jsx
@@ -3,14 +3,16 @@ import { Card } from "react-bootstrap";
 export function MenuCard({ meal, onSelect }) {
   return (
     <Card
-      className="p-2 mx-2 position-relative"
-      style={{ cursor: "pointer", width: "16rem" }}
-      onClick={() => onSelect(meal)}
+    className="p-2 mx-2 position-relative"
+    style={{ cursor: "pointer", width: "16rem" }}
+    onClick={() => onSelect(meal)}
     >
-      {comida.promocion && (
-        <span className="badge">¡Promoción!</span>
-      )}
-      <Card.Img variant="top" src={meal.strMealThumb} />
+      {/* { menuItem.is_promotion ? (
+        <span style={{ height: "2rem"}} className="position-absolute top-1 end-0 translate-middle-y badge rounded-pill bg-danger d-flex align-items-center"><span className="bg-light rounded-circle py-1 me-1">💯 </span>¡Promoción!</span>
+      ):  (menuItem.is_featured && (
+        <span style={{ height: "2rem"}} className="position-absolute top-1 end-0 translate-middle-y badge rounded-pill bg-warning text-dark d-flex align-items-center"><span className="bg-light rounded-circle py-1 me-1">⭐ </span> ¡Destacado!</span>
+      ))} */}
+      <Card.Img alt={meal.strMeal} variant="top" src={meal.strMealThumb} />
       <Card.Body>
         <Card.Title>{meal.strMeal}</Card.Title>
         <Card.Text>
