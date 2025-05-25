b228f0d8936b92d940d0c22a124028b7c13aa968
krmanrique <krmanriquesanchez@gmail.com>
2025-05-25 15:23:09 -0500

    feat: register models in admin.py for all apps and create superuser

--- backend/orders/admin.py
+++ backend/orders/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import OrderStatus, Order, OrderItemStatus, OrderItem, OrderItemOption
 
-# Register your models here.
+admin.site.register(OrderStatus)
+admin.site.register(Order)
+admin.site.register(OrderItemStatus)
+admin.site.register(OrderItem)
+admin.site.register(OrderItemOption)
