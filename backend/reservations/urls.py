2c387c2d4d5422651d03d0c8aafb21dfecda09c4
krmanrique <krmanriquesanchez@gmail.com>
2025-05-25 19:05:22 -0500

    fix: correction of reservation/urls.py

--- backend/reservations/urls.py
+++ backend/reservations/urls.py
@@ -5,7 +5,6 @@ from .views import ReservationViewSet, ReservationStatusViewSet
 
 router = DefaultRouter()
 router.register(r'', ReservationViewSet)
-router.register(r'table', TableViewSet)
 router.register(r'status', ReservationStatusViewSet)
 
 urlpatterns = [
