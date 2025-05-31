5512e9d8eea109e91a9c563408c7cdbdf8e888f0
Michael <stevenex13@gmail.com>
2025-05-31 15:00:22 -0500

    form tables create

--- frontend/src/routes/AdminRoutes.jsx
+++ frontend/src/routes/AdminRoutes.jsx
@@ -8,6 +8,7 @@ import { MenuAdminPage } from "../layouts/admin/pages/MenuAdminPage";
 import { MenuAdminForm } from "../layouts/admin/forms/MenuAdminForm";
 import { ItemAdminForm } from "../layouts/admin/forms/ItemAdminForm";
 import { ItemsAdminPage } from "../layouts/admin/pages/ItemsAdminPage";
+import { TableAdminForm } from "../layouts/admin/forms/TableAdminForm";
 import AdminLayout from "../layouts/admin/AdminLayout";
 import NotFound from "../common/Nofound";
 
@@ -25,6 +26,8 @@ const AdminRoutes = () => {
         <Route path="items/create" element={<ItemAdminForm />} />
         <Route path="items/edit/:id" element={<ItemAdminForm />} />
         <Route path="tables" element={<TablesAdminPage />} />
+        <Route path="tables/create" element={<TableAdminForm />} />
+        <Route path="tables/edit/:id" element={<TableAdminForm />} />
       </Route>
       <Route path="*" element={<NotFound />} /> {/* Este va fuera del layout */}
     </Routes>
